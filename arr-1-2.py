9a0754ae52e95fa1dd29f4e92e659c74c07cb758
fatemehrezaei <122838424+Fatemehrezaei79@users.noreply.github.com>
2023-01-20 01:46:32 +0330

    Add files via upload

---
+++ arr-1-2.py
@@ -0,0 +1,15 @@
+input_list =[]
+arr-1=[]
+arr-2=[]
+n=int(input("Enter number of elements : "))
+print("Enter elements : ")
+for i in range(0,n):
+    input_list.append(int(input();)
+for num in input_list :
+  if num %2==0:
+    arr-1.append(num)
+  else
+    arr-2.append(num)
+print("input_list:",input_list")
+print("arr-1:",arr-1")
+print("arr-2:",arr-2")
