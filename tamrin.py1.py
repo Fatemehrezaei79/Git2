a711ac9688e99e710f4a049852099989d4277372
Fatemehrezaei79 <rezaei1379.f@gmail.com>
2023-01-19 00:54:37 -0800

    added git hub

---
+++ tamrin.py1.py
@@ -0,0 +1,16 @@
+x="hi how are you Friend"
+print(x)
+num_h=len(x.split(" "))
+num_k=len(x)
+a=x.replace('t','p')
+print("num_h=",num_h)
+print("num_k=",num_k)
+print("Replace=",a)
+
+print()
+
+
+q="hi how are you ?,I am fine., whats up?,the health."
+num_j=len(q.split(","))
+print(q)
+print("num_j=",num_j)
